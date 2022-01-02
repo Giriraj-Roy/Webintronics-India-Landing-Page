075ce9f5b29f6c14bb16b18ccfa9d953be78a0dd
Giriraj Roy <girirajroy2001@gmail.com>
2022-01-03 00:44:07 +0530

    ContactBar Developed

---
+++ landing-page/src/components/ContactBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ContactBar = () => {
+    return (
+        <div className='contactBar'>
+            <div className='contactSupport'>
+                Support : +91 - 9090909090
+            </div>
+            <div className='contactTestRide'>
+                Book a FREE Test Ride Now
+            </div>
+        </div>
+    )
+}
+
+export default ContactBar
