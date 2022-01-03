3eb315f9fa73fe89760829d075bc2ea582d64327
Giriraj Roy <girirajroy2001@gmail.com>
2022-01-03 11:58:37 +0530

    navbar structured

--- landing-page/src/components/Navbar.js
+++ landing-page/src/components/Navbar.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div>
-            This is Navbar
+        <div className='navbar'>
+            <span className='rectOuter'><span className='rectInner'></span></span>
+            <div className='navPanel'>
+                <span>About</span>
+                <span>Locate Store</span>                
+                <span style={{marginRight: "20px", display: "inline-block"}}>Scooter <span className='mediaArrow'></span></span>
+                <span style={{marginRight: "20px", display: "inline-block"}}>Purchase Assistance <span className='mediaArrow'></span></span>
+                <button className="btnDealer">Dealership</button>
+            </div>
         </div>
     )
 }
