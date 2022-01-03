0166b4f3b245c8d7993bac22ef6b3548bed53826
Giriraj Roy <girirajroy2001@gmail.com>
2022-01-03 18:47:10 +0530

    made responsive

--- landing-page/src/components/ContactBar.js
+++ landing-page/src/components/ContactBar.js
@@ -3,14 +3,14 @@ import React from 'react'
 const ContactBar = () => {
     return (
         <div className='contactBar'>
-            <div style={{paddingLeft: '100px', paddingTop: '5px'}}>
+            <div style={{paddingLeft: '14vh', paddingTop: '0.7vh'}}>
                 Support : +91 - 9090909090
             </div>
             <span style={{color: "red", margin: "auto", marginLeft: "20%"}}>Book a FREE Test Ride Now</span>
             <div className='contactPanel' >
                 <span>Contact</span>
                 <span>Career</span>                
-                <span style={{marginRight: "20px", display: "inline-block"}}>Media <span className='mediaArrow'></span></span>
+                <span style={{marginRight: "3vh", display: "inline-block"}}>Media<span className='mediaArrow'></span></span>
             </div>
         </div>
     )
--- landing-page/src/components/Navbar.js
+++ landing-page/src/components/Navbar.js
@@ -5,10 +5,11 @@ const Navbar = () => {
         <div className='navbar'>
             <span className='rectOuter'><span className='rectInner'></span></span>
             <div className='navPanel'>
-                <span>About</span>
-                <span>Locate Store</span>                
-                <span style={{marginRight: "20px", display: "inline-block"}}>Scooter <span className='mediaArrow'></span></span>
-                <span style={{marginRight: "20px", display: "inline-block"}}>Purchase Assistance <span className='mediaArrow'></span></span>
+                <span className='navAbout'>About</span>
+                <span className='navStore'>Locate Store</span> 
+                               
+                <span style={{marginRight: "3vh", display: "inline-block"}}>Scooter  <span className='mediaArrow'></span></span>
+                <span style={{marginRight: "3vh", display: "inline-block"}}>Purchase Assistance  <span className='mediaArrow'></span></span>
                 <button className="btnDealer">Dealership</button>
             </div>
         </div>
